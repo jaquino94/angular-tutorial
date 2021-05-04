2c48122e515a0c66cb03b6e4901f0ac77577ac93
Justin Aquino <juaquino@tesla.com>
2021-05-04 15:04:22 -0700

    Init manual commit

---
+++ src/app/heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero"
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice', superpower: 'Telekinesis' },
+    { id: 12, name: 'Narco', superpower: 'Teleportation'},
+    { id: 13, name: 'Bombasto', superpower: 'Super bombs' },
+    { id: 14, name: 'Celeritas', superpower: 'Dark Magic' },
+    { id: 15, name: 'Magneta', superpower: 'Magnetism' },
+    { id: 16, name: 'RubberMan', superpower: 'Elasticity' },
+    { id: 17, name: 'Dynama', superpower: 'Cosmic Magic' },
+    { id: 18, name: 'Dr IQ', superpower: 'Psychic Power' },
+    { id: 19, name: 'Magma', superpower: 'Fire Beam' },
+    { id: 20, name: 'Tornado', superpower: 'Storm' }
+];
