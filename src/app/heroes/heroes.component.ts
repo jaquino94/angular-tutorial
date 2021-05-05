c4b9eb6105ad069698a94c1289f57e698070b2dd
Justin Aquino <juaquino@tesla.com>
2021-05-04 18:10:06 -0700

    Added Message, Hero service, and Messages component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { HEROES } from '../heroes';
 import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[] = [];
 
-  constructor() { 
+  constructor(private heroService: HeroService) { 
   }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
   
   selectedHero?: Hero;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
